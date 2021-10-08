3b9f7873af2e05ab314d6326bdbe365d80b03221
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-08 08:53:47 +0530

    Added: a new welcome file which can print welcome message on the console.

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package com.bridgelab.workshop.banking;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Second Java Workshop on the Topic of Banking.");
+	}
+
+}
